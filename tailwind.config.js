82fe3f1a3de66e3c237ba697f75869a19235c72f
Landon W. <lndnwar06@gmail.com>
2024-12-03 16:15:42 -0500

    Overall Landing Page Redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
       screens: {
         'smh': {'raw': '(min-height: 600px)'}, 
         'mdh': {'raw': '(min-height: 800px)'},
+      },
+      fontFamily: {
+        'sourgummy': ['Sour Gummy', 'sans-serif'],
+        'lacquer': ['Lacquer', 'sans-serif'],
       }
     },
   },
