87c64773106fa738df9541955c67d38a42a3b594
SoftwareNotion <lndnwar06@gmail.com>
2025-01-22 20:55:05 -0600

    Tours Responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       screens: {
         'smh': {'raw': '(min-height: 600px)'}, 
         'mdh': {'raw': '(min-height: 800px)'},
+        'mxl': {'raw': '(max-height: 1280px)'},
       },
       fontFamily: {
         'sourgummy': ['Sour Gummy', 'sans-serif'],
